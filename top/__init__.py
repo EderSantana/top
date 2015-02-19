8781d7301ddc156eea17c7a64454cd8ccbad983f
edersantana <edersantanajunior@hotmail.com>
2015-02-18 20:27:41 -0500

    Moved tests to a separate file.

--- top/__init__.py
+++ top/__init__.py
@@ -176,26 +176,4 @@ class Optimizer():
           cPickle.dump(pack,path,-1)
   '''
 
-def test1():
-  x = theano.shared(np.asarray(5.).astype(theano.config.floatX))
-  cost = T.sqr(x)
-  opt = Optimizer(x, cost, method='rmsprop', learning_rate=.1, momentum=.5,
-                  lr_rate=.998,m_rate=1.0001)
-  opt.run(5000)
-  print "Starting at x=5, after 5000 iterations, we found minimum of x**2 at x = %f" % opt.p.get_value()
 
-def test2():
-  b = T.vector()
-  A = theano.shared(np.random.randn(3,3).astype(theano.config.floatX))
-  x = theano.shared(np.zeros(3, dtype=theano.config.floatX))
-  c = theano.shared(np.ones(3, dtype=theano.config.floatX))
-  cost = T.sqr(b-T.dot(A,x)+c).sum()
-  opt = Optimizer(x, cost, input=b, method='sgd', learning_rate=.1, momentum=.5,
-                  lr_rate=.998,m_rate=1.0001)
-  bb = np.random.randn(3).astype(theano.config.floatX)
-  opt.run(5000,bb)
-  print "Expected"
-  print np.dot(np.linalg.inv(A.get_value()),bb+c.get_value())
-  print "Found"
-  print x.get_value()
-  print 'Try again!'
