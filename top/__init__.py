1d5d8fa74be00019d3c421ba28999b57a5d1ddb0
edersantana <edersantanajunior@hotmail.com>
2015-02-27 10:43:59 -0500

    Started refactoring; moved optimizer to its own file

--- top/__init__.py
+++ top/__init__.py
@@ -5,6 +5,7 @@ import theano.tensor as T
 import numpy as np
 from theano.ifelse import ifelse
 from theano.compat.python2x import OrderedDict
+from .optimizer import Optimizer
 
 floatX = theano.config.floatX
 def Adam(params, cost, lr=0.0002, b1=0.1, b2=0.001, e=1e-8, grad_clip=None):
@@ -19,7 +20,7 @@ def Adam(params, cost, lr=0.0002, b1=0.1, b2=0.001, e=1e-8, grad_clip=None):
     for p, g in zip(params, grads):
         if grad_clip is not None:
             gnorm = T.sqrt(T.sqr(g).sum())
-            ggrad = T.switch(T.ge(gnorm,grad_clip), 
+            ggrad = T.switch(T.ge(gnorm,grad_clip),
                              grad_clip*g/gnorm, g)
         else:
             ggrad = g
@@ -34,12 +35,12 @@ def Adam(params, cost, lr=0.0002, b1=0.1, b2=0.001, e=1e-8, grad_clip=None):
         updates.append((p, p_t))
     updates.append((i, i_t))
     return updates
-    
+
 def AdaGrad(params, cost, lr=1.0, eps=1e-6, lr_rate=None):
     """AdaGrad algorithm proposed was proposed in No More Pesky Learning
     Rates by Schaul et. al.
-    
-    :param params: list of :class:theano.shared variables to be optimized 
+
+    :param params: list of :class:theano.shared variables to be optimized
     :param cost: cost function that should be minimized in the optimization
     :param float lr: learning rate
     :param float eps: small constant to avoid division by zero
@@ -56,15 +57,15 @@ def AdaGrad(params, cost, lr=1.0, eps=1e-6, lr_rate=None):
     return lr_m_schedule(updates, lr, None, lr_rate, None)
 
 def Adam2(params, cost, lr=0.0002, b1=0.1, b2=0.001, e=1e-8):
-    """Adam algorithm proposed was proposed in Adam: A Method for Stochastic 
+    """Adam algorithm proposed was proposed in Adam: A Method for Stochastic
     Optimization.
     This code was modified from Newmu's code:
     https://gist.github.com/Newmu/acb738767acb4788bac3
-    
-    :param params: list of :class:theano.shared variables to be optimized 
+
+    :param params: list of :class:theano.shared variables to be optimized
     :param cost: cost function that should be minimized in the optimization
     :param float lr: learning rate
-    :param float b1: ToDo: WRITEME 
+    :param float b1: ToDo: WRITEME
     :param float b2: ToDo: WRITEME
     :param float e: ToDO: WRITEME
     """
@@ -76,12 +77,12 @@ def Adam2(params, cost, lr=0.0002, b1=0.1, b2=0.001, e=1e-8):
     b1t = 1. - (1. - b1)*e**(-i)
     for p, g in zip(params, grads):
         m = theano.shared(p.get_value() * 0.)
-        v = theano.shared(p.get_value() * 0.) 
+        v = theano.shared(p.get_value() * 0.)
         m_t = (b1t * g) + ((1. - b1t) * m)
         v_t = (b2 * T.sqr(g)) + ((1. - b2) * v)
         m_h = m_t / (1-(1-b1)**i_t)
         v_h = v_t / (1-(1-b2)**i_t)
-        p_t = p - lr * m_h / (T.sqrt(v_h) + e) 
+        p_t = p - lr * m_h / (T.sqrt(v_h) + e)
         updates.append((m,m_t))
         updates.append((v,v_t))
         updates.append((p,p_t))
@@ -90,7 +91,7 @@ def Adam2(params, cost, lr=0.0002, b1=0.1, b2=0.001, e=1e-8):
 
 def rmsprop(parameters,cost=None,gradients=None,
            updates=None,lr=2e-2, consider_constant = [], epsilon=1e-8,
-           momentum = None, lr_rate=None, m_rate=None, 
+           momentum = None, lr_rate=None, m_rate=None,
            grad_clip=None, **kwargs):
 
     rho = .9
@@ -106,7 +107,7 @@ def rmsprop(parameters,cost=None,gradients=None,
     for param,grad in zip(parameters,grads):
         if grad_clip is not None:
             gnorm = T.sqrt(T.sqr(grad).sum())
-            ggrad = T.switch(T.ge(gnorm,grad_clip), 
+            ggrad = T.switch(T.ge(gnorm,grad_clip),
                              grad_clip*grad/gnorm, grad)
         else:
             ggrad = grad
@@ -119,7 +120,7 @@ def rmsprop(parameters,cost=None,gradients=None,
             scale = kwargs['scale']
         if momentum != None:
             mparam = theano.shared(param.get_value()*0.)
-            v = momentum * mparam - lr * ggrad/ T.sqrt(new_accum + epsilon) 
+            v = momentum * mparam - lr * ggrad/ T.sqrt(new_accum + epsilon)
             w = param + momentum * v - lr * ggrad/ T.sqrt(new_accum + epsilon)
             updates.append((mparam, v))
             updates.append((param, w))
@@ -162,73 +163,7 @@ def lr_m_schedule(updates, lr, momentum, lr_rate=None, m_rate=None):
     updates.append((momentum, ifelse(momentum*m_rate>.9,.9,momentum*m_rate))) #T.maximum(momentum * m_rate, .99)
   return updates
 
-class Optimizer():
-  """Optimizer API
-  """
-  def __init__(self, parameters, cost, method='sgd',input=[], givens=None,
-               constant=None,learning_rate=.001, momentum=None,
-               lr_rate=None, m_rate=None, extra_updates=None,
-               grad_clip=None):
-    if not isinstance(parameters,list):
-        parameters = [parameters]
-    self.p = parameters
-    
-    self.cost = cost
-    self.lr = theano.shared(np.asarray(learning_rate, dtype=theano.config.floatX))
-    if momentum:
-        self.m = theano.shared(np.asarray(momentum, dtype=theano.config.floatX))
-    else:
-        self.m = None
-    self.input = input
-    self.lr_rate = lr_rate
-    self.m_rate = m_rate
-    self.method = method
-    self.givens = givens
-    self.cc = constant
-    self.extra_updates = extra_updates
-    self.grad_clip = grad_clip
 
-  def compile(self):
-    print "$> Compiling optimizer."
-
-    if self.method.lower() == 'sgd':
-      updates = sgd(self.p, cost=self.cost, lr=self.lr, momentum=self.m,
-                   lr_rate=self.lr_rate, m_rate=self.m_rate, consider_cosntant=self.cc)
-    elif self.method.lower() == 'rmsprop':
-      updates = rmsprop(self.p,self.cost, lr=self.lr, momentum=self.m,
-                        lr_rate=self.lr_rate, m_rate=self.m_rate, 
-                        consider_constant=self.cc, grad_clip=self.grad_clip)
-    elif self.method.lower() == 'adam':
-      updates = Adam(self.p, self.cost, lr=self.lr, grad_clip=self.grad_clip)
-    elif self.method.lower() == 'adagrad':
-      updates = AdaGrad(self.p, self.cost, lr=self.lr, lr_rate=self.lr_rate)
-    else:
-      raise NotImplementedError("Optimization method not implemented!")
-    
-    if self.extra_updates is not None:
-        updates.append(self.extra_updates)
-    
-    updates = OrderedDict(updates)
-
-    if self.input == []:
-        self.f = theano.function([], self.cost, updates=updates, givens=self.givens, allow_input_downcast=True)
-        self.g = theano.function([], self.cost, givens=self.givens, allow_input_downcast=True)
-    else:
-      if not isinstance(self.input,list):
-          self.input = [self.input]
-      self.f = theano.function(self.input, self.cost, updates=updates, givens=self.givens, allow_input_downcast=True)
-      self.g = theano.function(self.input, self.cost, givens=self.givens, allow_input_downcast=True)
-
-    return self
-
-  def run(self,niter,*args):
-    total = 0.
-    if not hasattr(self,'f'):
-      self.compile()
-    for k in range(niter):
-      total += self.f(*args)
-    return self, total
-  
   '''
   TODO maybe? Add validate_n_save method
   def validate_and_save(valid, path):
@@ -238,5 +173,3 @@ class Optimizer():
           pack = {'parameters': self.p, 'iter': iter, 'cost': self.best_cost}
           cPickle.dump(pack,path,-1)
   '''
-
-
